c92c7e682c75e99ca7c656e0745ef5e1c833c9e7
Norgius <dmitry.schadrin21@yandex.ru>
2023-09-06 15:38:54 +0300

    Initialize settings for project

--- settings.py
+++ settings.py
@@ -2,6 +2,12 @@ from pydantic import Field, SecretStr, EmailStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+class RedisConfig(BaseSettings):
+    HOST: str
+    PORT: int
+    PASSWORD: SecretStr
+
+
 class Settings(BaseSettings):
     SMSC_LOGIN: str
     SMSC_PASSWORD: SecretStr
@@ -10,7 +16,15 @@ class Settings(BaseSettings):
     MESSAGE_SUBJECT: str = Field(default='Погода')
     MESSAGE_LIFE_SPAN: int = Field(default=1)
 
-    model_config = SettingsConfigDict(env_file='.env', extra='ignore')
+    REDIS: RedisConfig
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        extra='ignore',
+        case_sensitive=True,
+        env_nested_delimiter='__',
+        validate_default=True
+    )
 
 
 ENV = Settings()
